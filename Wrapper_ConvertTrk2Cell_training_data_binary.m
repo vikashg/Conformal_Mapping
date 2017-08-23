c95dad45756c7909f2dbeed126194b19e4ecefaf
Vikash Gupta <vgupta@c2001.c.ini.usc.edu>
2017-08-23 16:20:07 -0700

    Wrappers for binary classification added

--- Wrapper_ConvertTrk2Cell_training_data_binary.m
+++ Wrapper_ConvertTrk2Cell_training_data_binary.m
@@ -4,15 +4,20 @@ readInputFile;
 input_file_other_subjs;
 
 % trk_list is defined in the input_file_other_subjs;
-output_dir_tracks_seg=strcat(output_dir_tracks, '/Segmented_tracks/');
+roi_tracks_dir=trk_file_path
+% output_dir_tracks is all good 
 
-if (exist(output_dir_tracks_seg) == 0)
-    mkdir(output_dir_tracks_seg)
+
+if (exist(output_dir_tracks) == 0)
+    mkdir(output_dir_tracks)
 end
+
+output_dir_tracks_seg = output_dir_tracks;
 
 for i = 1: length(trk_list)
 
-    trk_file=strcat(roi_tracks_dir,trk_list{i}, '.trk')
+% Kept
+    trk_file=strcat(roi_tracks_dir,trk_list{i},  '_keptByFaisal.trk')
     [header, tracks] = trk_read(trk_file);
 
     tracks_new = trk_interp(tracks, number_of_points_per_track); % resampling the track
@@ -20,9 +25,23 @@ for i = 1: length(trk_list)
     disp('Read Track file ');
 
     track_cell = ConvertTrk2Cell(tracks_interp_str, Padding, voxDim);
-    file_name=strcat(output_dir_tracks_seg, trk_list{i}, '.mat')
+    file_name=strcat(output_dir_tracks_seg, trk_list{i},'_kept', '.mat')
     save(file_name, 'track_cell');
     output_string = strcat('Saved track ', trk_list{i});
     disp(output_string);
+
+%Removed
+    trk_file=strcat(roi_tracks_dir,trk_list{i},  '_removedByFaisal.trk')
+    [header, tracks] = trk_read(trk_file);
+
+    tracks_new = trk_interp(tracks, number_of_points_per_track); % resampling the track
+    tracks_interp_str = trk_restruc(tracks_new);
+    disp('Read Track file ');
+
+    track_cell = ConvertTrk2Cell(tracks_interp_str, Padding, voxDim);
+    file_name=strcat(output_dir_tracks_seg, trk_list{i},'_removed', '.mat')
+    save(file_name, 'track_cell');
+
+
 end
     disp('All trks converted');
